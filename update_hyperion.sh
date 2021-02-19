41f7fd86c7c33b1d253ec4f1d658d87e961c83a5
dan-froe <78253223+dan-froe@users.noreply.github.com>
2021-02-19 11:21:25 +0100

    test compile detection

--- update_hyperion.sh
+++ update_hyperion.sh
@@ -78,7 +78,7 @@ if [ $OS_LIBREELEC -eq 1 ]; then
 	echo 'We are on LibreELEC'
 	OS=$(lsb_release -i | cut -d : -f 2)
 	found_compile=1
-	sudo find / -path "*/.git/config" | cat "config" | grep "/hyperion-project/hyperion.ng.git" && found_compile=0
+	sudo find / -path "*/.git/config" | cat "config" | grep "/hyperion-project/hhyperion.ng.git" && found_compile=0
 # Stop hyperion service if it is running
 	systemctl -q stop hyperion.service >/dev/null 2>/dev/null
 	systemctl -q stop hyperiond@pi.service >/dev/null 2>/dev/null
