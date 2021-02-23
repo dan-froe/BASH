92ca0061ffc55003cf29998e492ab74566bdd174
dan-froe <d.froebe77@gmail.com>
2021-02-23 13:32:41 +0100

    merge hyperion_auto w/master

---
+++ hyperion_auto/installation.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#delete dublicates
+rm ./instanzen.sh >/dev/null 2>&1
+rm ./instanzen2.sh >/dev/null 2>&1
+#download scripts
+wget -qL - https://raw.githubusercontent.com/dan-froe/BASH/overhaul/hyperion_auto/instanzen.sh
+wget -qL - https://raw.githubusercontent.com/dan-froe/BASH/overhaul/hyperion_auto/instanzen2.sh
+#make executable
+sudo chmod +x ./instanzen.sh
+sudo chmod +x ./instanzen2.sh
+#write out current crontab
+crontab -l > mycron
+#test for duplication
+[[ $(cat mycron | grep instanzen.sh | cut -d " " -f 4,4 | cut -d "/" -f 4,4) = "instanzen.sh" ]] && echo && echo && echo && echo $'\033[0;32mCommand found in crontab. No update needed!' && echo && echo && echo && rm mycron && exit 0
+#echo new cron into cron file
+dir=$(pwd)/instanzen.sh
+echo "@reboot sudo bash $dir >ok >error" >> mycron
+#install new cron file
+crontab mycron
+rm mycron
+echo; echo; echo; echo; echo $'\033[0;32mI updated crontab. Everything is ready!'; echo; echo; echo
---
+++ hyperion_auto/instanzen2.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+
+#silent boot
+#exec >ok
+#exec 2>error
+
+#variables
+#var=0
+#i=0
+
+#########################################################################
+#check if hyperiond is running
+#while [[ $var != "active(running)" ]] && [[ $i < "4" ]]
+#do	
+#	i=$(($i+1))
+#	var=$(systemctl status hyperiond* | grep 'active (running)' | sed -e 's/Active://' -e 's/since.*ago//' | tr -d " ")
+#	sleep 5
+#done
+#########################################################################
+
+
+#sleep 20
+
+
+
+#instance 1/2 on,LED on, V4l on
+#CHANGE LOCALHOST TO IP OF PI
+curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "startInstance","instance" : 1}'
+
+curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "startInstance","instance" : 2}'
+
+curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "switchTo","instance" : 1}' --next 'http://localhost:8090/json-rpc' --data '{"command":"componentstate","componentstate":{"component":"LEDDEVICE","state":true}}'
+
+curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "switchTo","instance" : 2}' --next 'http://localhost:8090/json-rpc' --data '{"command":"componentstate","componentstate":{"component":"LEDDEVICE","state":true}}'
+
+curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "switchTo","instance" : 1}' --next 'http://localhost:8090/json-rpc' --data '{"command":"componentstate","componentstate":{"component":"V4L","state":true}}'
+
+curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "switchTo","instance" : 2}' --next 'http://localhost:8090/json-rpc' --data '{"command":"componentstate","componentstate":{"component":"V4L","state":true}}'
+
+curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "switchTo","instance" : 1}' --next 'http://localhost:8090/json-rpc' --data '{"command":"componentstate","componentstate":{"component":"GRABBER","state":false}}'
+
+curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "switchTo","instance" : 2}' --next 'http://localhost:8090/json-rpc' --data '{"command":"componentstate","componentstate":{"component":"GRABBER","state":false}}'
