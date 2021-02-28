577553920d41c6a3cd375c967f1f3b4bb7e5f134
dan-froe <78253223+dan-froe@users.noreply.github.com>
2021-02-28 17:27:09 +0100

    Create install_new.sh

---
+++ hyperion_auto/install_new.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+
+#silent boot
+#exec >ok
+#exec 2>error
+
+#variables
+var=0
+i=0
+
+#########################################################################
+#check if hyperiond is running
+while [[ $var != "active(running)" ]] && [[ $i < "4" ]]
+do	
+	i=$(($i+1))
+	var=$(systemctl status hyperiond* | grep 'active (running)' | sed -e 's/Active://' -e 's/since.*ago//' | tr -d " ")
+	sleep 5
+done
+#########################################################################
+
+
+#sleep 20
+
+curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "switchTo","instance" : 0}' --next 'http://localhost:8090/json-rpc' --data '{"command":"componentstate","componentstate":{"component":"LEDDEVICE","state":true}}'
+
+
+#Diese Zeile kann auch verwendet werden, dann einfach in den crontab kopieren.
+#Der Befehl wartet 20 Sekunden, damit Hyperion gestartet ist und es funktioniert. 
+#Du kannst die Sekunden erhöhen oder verringern wie es am Besten passt. 
+#ABER: Ohne # am Anfang eintragen! 
+#@reboot sleep 20 && curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "switchTo","instance" : 0}' --next 'http://localhost:8090/json-rpc' --data '{"command":"componentstate","componentstate":{"component":"LEDDEVICE","state":true}}'
