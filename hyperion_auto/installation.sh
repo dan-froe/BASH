e5d59d9fa0c8177fff6647798fa60deadd416941
dan-froe <78253223+dan-froe@users.noreply.github.com>
2021-03-04 15:19:12 +0100

    engl translation

--- hyperion_auto/installation.sh
+++ hyperion_auto/installation.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 #delete dublicates
-rm ./instanzen.sh >/dev/null 2>&1
-rm ./instanzen2.sh >/dev/null 2>&1
+rm ./instance.sh >/dev/null 2>&1
+rm ./instance2.sh >/dev/null 2>&1
 #download scripts
-wget -qL - https://raw.githubusercontent.com/dan-froe/BASH/master/hyperion_auto/instanzen.sh
-wget -qL - https://raw.githubusercontent.com/dan-froe/BASH/master/hyperion_auto/instanzen2.sh
+wget -qL - https://raw.githubusercontent.com/dan-froe/BASH/master/hyperion_auto/instance.sh
+wget -qL - https://raw.githubusercontent.com/dan-froe/BASH/master/hyperion_auto/instance2.sh
 #make executable
-sudo chmod +x ./instanzen.sh
-sudo chmod +x ./instanzen2.sh
+sudo chmod +x ./instance.sh
+sudo chmod +x ./instance2.sh
 #write out current crontab
 crontab -l > mycron
 #test for duplication
-[[ $(cat mycron | grep instanzen.sh | cut -d " " -f 4,4 | cut -d "/" -f 4,4) = "instanzen.sh" ]] && echo && echo && echo && echo $'\033[0;32mCommand found in crontab. No update needed!' && echo && echo && echo && rm mycron && exit 0
+[[ $(cat mycron | grep instance.sh | cut -d " " -f 4,4 | cut -d "/" -f 4,4) = "instance.sh" ]] && echo && echo && echo && echo $'\033[0;32mCommand found in crontab. No update needed!' && echo && echo && echo && rm mycron && exit 0
 #echo new cron into cron file
-dir=$(pwd)/instanzen.sh
+dir=$(pwd)/instance.sh
 echo "@reboot sudo bash $dir >ok >error" >> mycron
 #install new cron file
 crontab mycron
