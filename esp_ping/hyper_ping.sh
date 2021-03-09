c6e69c8652990b0b5e99bf314736291ab3f53e9b
dan-froe <78253223+dan-froe@users.noreply.github.com>
2021-03-09 14:50:26 +0100

    Update hyper_ping.sh

--- esp_ping/hyper_ping.sh
+++ esp_ping/hyper_ping.sh
@@ -10,12 +10,12 @@ while :
 do
    ping -c "$IP" >/dev/null 2>&1
    var="$?"
-   if [[ "var" = "0" ]] && [[ "i" = "0" ]]; then
+   if [[ "$var" = "0" ]] && [[ "$i" = "0" ]]; then
        curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "startInstance","instance" : 1}' >/dev/null 2>&1
        curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "switchTo","instance" : 1}' --next 'http://localhost:8090/json-rpc' --data '{"command":"componentstate","componentstate":{"component":"LEDDEVICE","state":true}}' >/dev/null 2>&1
        i=1
        echo 'ping successful' >>bar 2>&1
-   elif [[ "var" = "0" ]] && [[ "i" = "1" ]]; then
+   elif [[ "$var" = "0" ]] && [[ "$i" = "1" ]]; then
        echo 'ping still successful' >>bar 2>&1
    else
        echo 'no answer' >>bar 2>&1
