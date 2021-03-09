e9ac3726a4ca5831007ab7ee735326bc446c8c5c
dan-froe <78253223+dan-froe@users.noreply.github.com>
2021-03-09 20:01:58 +0100

    Update hyper_ping.sh

--- esp_ping/hyper_ping.sh
+++ esp_ping/hyper_ping.sh
@@ -6,12 +6,12 @@ var="0"
 IP="$1"
 time_sec="$2"
 sleep_long="30"
-is_on="0"
+is_on="false"
 
 #endless loop
 while :
 do
-   # ping ESP device
+   #ping ESP device
    ping -c 1 -w 1 "$IP" >/dev/null 2>&1
    var="$?"
 
@@ -23,20 +23,20 @@ do
           curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "startInstance","instance" : 1}' >/dev/null 2>&1
           curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "switchTo","instance" : 1}' --next 'http://localhost:8090/json-rpc' --data '{"command":"componentstate","componentstate":{"component":"LEDDEVICE","state":true}}' >/dev/null 2>&1
           is_on=$(curl -s -X POST -i http://localhost:8090/json-rpc --data '{"command": "serverinfo", "tan":1}' | grep -A1 '"instance": 1' | grep -v '"instance"' | sed 's/"running": //' | tr -d ' ')
-          echo 'no instance' >>bar 2>&1
+#         echo 'no instance' >>bar 2>&1
        done
        i=1
-       echo 'ping successful' >>bar 2>&1
+#      echo 'ping successful' >>bar 2>&1
        sleep $sleep_long
 
    #second to n successful answers
    elif [[ "$var" = "0" ]] && [[ "$i" = "1" ]]; then
-       echo 'ping still successful' >>bar 2>&1
+#      echo 'ping still successful' >>bar 2>&1
        sleep $sleep_long
 
    #no one home
    else
-       echo 'no answer' >>bar 2>&1
+#      echo 'no answer' >>bar 2>&1
        i=0
    fi
 
