a03a1abadacbcce5732c636451a353aeb68a2d47
dan-froe <78253223+dan-froe@users.noreply.github.com>
2021-03-09 15:13:51 +0100

    wait 5sec after first success

--- esp_ping/hyper_ping.sh
+++ esp_ping/hyper_ping.sh
@@ -18,6 +18,7 @@ do
        curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "startInstance","instance" : 1}' >/dev/null 2>&1
        curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "switchTo","instance" : 1}' --next 'http://localhost:8090/json-rpc' --data '{"command":"componentstate","componentstate":{"component":"LEDDEVICE","state":true}}' >/dev/null 2>&1
        i=1
+       sleep 5
        echo 'ping successful' >>bar 2>&1
    
    #second to n successful answers
