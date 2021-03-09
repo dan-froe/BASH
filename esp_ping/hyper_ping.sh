63349065a53f3ca4e7a4c89e0e68e59f92aaa634
dan-froe <78253223+dan-froe@users.noreply.github.com>
2021-03-09 19:30:31 +0100

    Update hyper_ping.sh

--- esp_ping/hyper_ping.sh
+++ esp_ping/hyper_ping.sh
@@ -23,19 +23,20 @@ do
           curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "startInstance","instance" : 1}' >/dev/null 2>&1
           curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "switchTo","instance" : 1}' --next 'http://localhost:8090/json-rpc' --data '{"command":"componentstate","componentstate":{"component":"LEDDEVICE","state":true}}' >/dev/null 2>&1
           is_on=$(curl -s -X POST -i http://localhost:8090/json-rpc --data '{"command": "serverinfo", "tan":1}' | grep -A1 '"instance": 1' | grep -v '"instance"' | sed 's/"running": //' | tr -d ' ')
+          echo no instance
        done
        i=1
-#      echo 'ping successful' >>bar 2>&1
+       echo 'ping successful' >>bar 2>&1
        sleep $sleep_long
 
    #second to n successful answers
    elif [[ "$var" = "0" ]] && [[ "$i" = "1" ]]; then
-#      echo 'ping still successful' >>bar 2>&1
+       echo 'ping still successful' >>bar 2>&1
        sleep $sleep_long
 
    #no one home
    else
-#      echo 'no answer' >>bar 2>&1
+       echo 'no answer' >>bar 2>&1
        i=0
    fi
 
