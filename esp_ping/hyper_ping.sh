7a0968eb592681ffd341a3244214eb3a5c952475
dan-froe <78253223+dan-froe@users.noreply.github.com>
2021-03-09 19:02:08 +0100

    Update hyper_ping.sh

--- esp_ping/hyper_ping.sh
+++ esp_ping/hyper_ping.sh
@@ -6,6 +6,7 @@ var="0"
 IP="$1"
 time_sec="$2"
 sleep_long="30"
+is_on="0"
 
 #endless loop
 while :
@@ -16,9 +17,13 @@ do
 
    #first success after error
    if [[ "$var" = "0" ]] && [[ "$i" = "0" ]]; then
-       sleep 1
-       curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "startInstance","instance" : 1}' >/dev/null 2>&1
-       curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "switchTo","instance" : 1}' --next 'http://localhost:8090/json-rpc' --data '{"command":"componentstate","componentstate":{"component":"LEDDEVICE","state":true}}' >/dev/null 2>&1
+       while [[ "$is_on" -ne "true" ]]
+       do
+          sleep 1
+          curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "startInstance","instance" : 1}' >/dev/null 2>&1
+          curl -i -X POST 'http://localhost:8090/json-rpc' --data '{"command" : "instance","subcommand" : "switchTo","instance" : 1}' --next 'http://localhost:8090/json-rpc' --data '{"command":"componentstate","componentstate":{"component":"LEDDEVICE","state":true}}' >/dev/null 2>&1
+          is_on=$(curl -s -X POST -i http://localhost:8090/json-rpc --data '{"command": "serverinfo", "tan":1}' | grep -A1 '"instance": 1' | grep -v '"instance"' | sed 's/"running": //' | tr -d ' ')
+       done
        i=1
 #      echo 'ping successful' >>bar 2>&1
        sleep $sleep_long
