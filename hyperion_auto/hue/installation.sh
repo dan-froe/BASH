8e7e4ac2235b20fa10be673dab95928c45f2cf69
dan-froe <78253223+dan-froe@users.noreply.github.com>
2021-04-17 12:06:12 +0200

    Update installation.sh

--- hyperion_auto/hue/installation.sh
+++ hyperion_auto/hue/installation.sh
@@ -13,11 +13,9 @@ rm ./instance2.sh >/dev/null 2>&1
 
 #download scripts
 wget -qL - https://raw.githubusercontent.com/dan-froe/BASH/master/hyperion_auto/hue/instance.sh
-#wget -qL - https://raw.githubusercontent.com/dan-froe/BASH/master/hyperion_auto/hue/instance2.sh
 
 #make executable
 sudo chmod +x ./instance.sh
-#sudo chmod +x ./instance2.sh
 
 #write out current crontab
 crontab -l > mycron
@@ -40,8 +38,6 @@ done
 cat mycron | grep "@reboot sudo bash $(pwd)/instance.sh" >/dev/null 2>&1
 foo="$?"
 [[ "$foo" = "0" ]] && echo && echo && echo && echo $'\033[0;32mCommand found in crontab. No update needed!' && echo && echo && echo && mycron && exit 0
-
-#[[ $(cat mycron | grep instance.sh | cut -d " " -f 4,4 | cut -d "/" -f 4,4) = "instance.sh" ]] && echo && echo && echo && echo $'\033[0;32mCommand found in crontab. No update needed!' && echo && echo && echo && rm mycron && exit 0
 
 #new cron into cron file
 dir=$(pwd)/instance.sh
